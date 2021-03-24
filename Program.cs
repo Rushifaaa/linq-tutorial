829b23a4b52bd31b20ace1a6c19c66d6df1c3dd8
Rushifaaa <jakubgencer@protonmail.com>
2021-03-24 10:22:25 +0100

    Linq Tutorial Part 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqTest();
+        }
+
+        static void LinqTest()
+        {
+            string[] names = { "Hans", "Peter", "Mustername", "Manfred", "Günther", "Gustav" };
+            var linqTest = from name in names where name.Contains("t") select name;
+
+            foreach (string name in linqTest)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
